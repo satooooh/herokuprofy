da8c250fc3534ac2f746d786d7ea402e2e0f88d3
satooooh <st.tomonori@gmail.com>
2017-06-09 19:39:21 +0900

    一問一答形式にする

--- app/controllers/answer_controller.rb
+++
@@ -1,9 +0,0 @@
-class AnswerController < ApplicationController
-
-  def new
-    @question = Question.find(params[:question_id])
-    @answer = Answr.new
-    @answer.question_id = @question.id
-  end
-
-end
