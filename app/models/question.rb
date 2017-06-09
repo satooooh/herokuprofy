03bc45d7e7113ff1574bb62a16a027059210990e
satooooh <st.tomonori@gmail.com>
2017-06-09 20:47:32 +0900

    ユーザーページに答えた質問を追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,8 +2,11 @@ class Question < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :group
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   validates_presence_of :user_id, :text, :group_id
 
+  def user_answer(user_id)
+    Answer.find_by(user_id: user_id, question_id: id)
+  end
 end
