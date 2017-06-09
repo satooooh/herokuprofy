a6f1fbcfd4891a34fe3d5d00c603201bd373f07c
satooooh <st.tomonori@gmail.com>
2017-06-09 22:53:04 +0900

    timeline2終了

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ActiveRecord::Base
   has_many :users
-  has_many :questions, ->{ order("created_at DESC") }
+  has_many :questions, ->{ order("updated_at DESC") }
+  has_many :feed_contents, ->{ order("updated_at DESC") }
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,21 @@
 class Question < ActiveRecord::Base
 
+  after_create :create_feed_content
+
   belongs_to :user
   belongs_to :group
   has_many :answers, dependent: :destroy
+  has_one :feed_content, as: :content, dependent: :destroy
 
   validates_presence_of :user_id, :text, :group_id
 
   def user_answer(user_id)
     Answer.find_by(user_id: user_id, question_id: id)
   end
+
+  private
+  def create_feed_content
+    self.feed_content = FeedContent.create(group_id: group_id, updated_at: updated_at)
+  end
+
 end
