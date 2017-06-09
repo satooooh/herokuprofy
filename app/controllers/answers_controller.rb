12cf05db73642f44b4f3231603a8dc061e608231
satooooh <st.tomonori@gmail.com>
2017-06-09 20:55:50 +0900

    timeline1終了

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,6 +12,17 @@ class AnswersController < ApplicationController
     @answer = Answer.create(create_params)
   end
 
+  def edit
+    @answer = Answer.find(params[:id])
+    @question = @answer.question
+  end
+
+  def update
+    @answer = Answer.find(params[:id])
+    @question = @answer.question
+    @answer.update(update_params)
+  end
+
   private
   def create_params
     params.require(:answer).permit(:question_id, :text).merge(user_id: current_user.id)
@@ -21,6 +32,10 @@ class AnswersController < ApplicationController
     if Answer.exists?(question_id: params[:question_id], user_id: current_user.id)
       redirect_to :root
     end
+  end
+
+  def update_params
+    params.require(:answer).permit(:text)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: "top#index"
   resources :groups, only: [:show, :edit, :update]
   resources :questions, only: [:create, :show]
-  resources :answers, only: [:new, :create]
+  resources :answers, only: [:new, :create, :edit, :update]
 end
